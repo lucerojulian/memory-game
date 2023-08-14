a018e67b8cce217e148f42ba71dc10c2090fe83d
Julian Lucero <julianlucero2012@gmail.com>
2023-08-14 14:17:59 -0300

    chore: Updated congratulations styles

--- src/components/game-board/components/CongratulationsMessage.tsx
+++ src/components/game-board/components/CongratulationsMessage.tsx
@@ -2,26 +2,21 @@ import { FC } from 'react';
 import PrimaryButton from '../../../shared/controls/PrimaryButton';
 import ConfettiExplosion from 'react-confetti-explosion';
 import TrophyIcon from '../../../shared/icons/TrophyIcon';
+import useUser from '../../../hooks/useUser';
 
 interface Props {
   handleGoToMenu: () => void;
 }
 
 const CongratulationsMessage: FC<Props> = ({ handleGoToMenu }) => {
+  const { name } = useUser();
+
   return (
     <>
-      <div className="fixed z-10 w-screen h-screen flex items-center justify-center flex-col gap-4 bg-black/80 top-0 left-0">
+      <div className="fixed z-10 w-screen h-screen flex items-center justify-center flex-col gap-4 top-0 left-0 bg-black/90">
         <TrophyIcon className="h-40 fill-yellow-400 animate-bounce" />
-        <h2 className="text-center font-sans font-bold mb-16">
-          <span className="text-5xl text-purple-500 ">C</span>
-          <span className="text-5xl text-green-500 ">o</span>
-          <span className="text-5xl text-cyan-500 ">n</span>
-          <span className="text-5xl text-yellow-500 ">g</span>
-          <span className="text-5xl text-red-500 ">r</span>
-          <span className="text-5xl text-teal-500 ">a</span>
-          <span className="text-5xl text-violet-500 ">t</span>
-          <span className="text-5xl text-lime-500 ">s</span>
-          <span className="text-5xl text-fuchsia-500 ">!</span>
+        <h2 className="text-center text-yellow-500 text-4xl sm:text-5xl font-sans font-bold mb-16 bg-white/10 border-y border-yellow-500 p-4 w-full">
+          Congratulations {name}!
         </h2>
         <PrimaryButton
           type="button"
