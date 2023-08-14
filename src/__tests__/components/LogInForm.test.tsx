8f54bd2a64a47fac213b0af250ff1385f8b82cd8
Julian Lucero <julianlucero2012@gmail.com>
2023-08-14 16:42:21 -0300

    tests: Added LoginForm tests

---
+++ src/__tests__/components/LogInForm.test.tsx
@@ -0,0 +1,54 @@
+import { fireEvent, render, screen } from '@testing-library/react';
+import LogInForm from '../../components/LogInForm';
+
+describe('LoginForm', () => {
+  it('should render required elements to log in', () => {
+    render(<LogInForm onLogIn={() => {}} />);
+
+    const loginMessage = screen.queryByText(/Please log in to play!/i);
+    const nameInput = screen.queryByPlaceholderText(/Enter your name/i);
+    const submitButton = screen.queryByText(/Play/);
+
+    expect(loginMessage).toBeVisible();
+    expect(nameInput).toBeVisible();
+    expect(submitButton).toBeVisible();
+  });
+
+  it('should change input value when typing', () => {
+    render(<LogInForm onLogIn={() => {}} />);
+
+    const nameInput = screen.queryByPlaceholderText(
+      /Enter your name/i,
+    ) as HTMLInputElement;
+
+    fireEvent.change(nameInput, { target: { value: 'julian' } });
+    expect(nameInput).toHaveValue('julian');
+  });
+
+  it('should execute onLogin correctly', () => {
+    const mockOnLogin = vi.fn();
+
+    render(<LogInForm onLogIn={mockOnLogin} />);
+
+    const submitButton = screen.queryByText(/Play/) as HTMLButtonElement;
+    const nameInput = screen.queryByPlaceholderText(
+      /Enter your name/i,
+    ) as HTMLInputElement;
+
+    fireEvent.change(nameInput, { target: { value: 'julian' } });
+    fireEvent.click(submitButton);
+
+    expect(mockOnLogin).toHaveBeenCalledTimes(1);
+  });
+
+  it('should not execute the onLogin if the form is empty', () => {
+    const mockOnLogin = vi.fn();
+
+    render(<LogInForm onLogIn={mockOnLogin} />);
+
+    const submitButton = screen.queryByText(/Play/) as HTMLButtonElement;
+    fireEvent.click(submitButton);
+
+    expect(mockOnLogin).toHaveBeenCalledTimes(0);
+  });
+});
