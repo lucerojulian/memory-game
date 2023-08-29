44736a68dd78483cd8fbd25d84407d9b27dcb7f5
Julian Lucero <julianlucero2012@gmail.com>
2023-08-29 18:07:22 -0300

    chore: Fixed double <main> at App

--- src/App.tsx
+++ src/App.tsx
@@ -5,12 +5,12 @@ import Nav from './components/Nav';
 const App = () => {
   return (
     <Providers>
-      <main className="relative h-screen grid grid-rows-[auto,_1fr] overflow-hidden max-w-5xl m-auto">
+      <div className="relative h-screen grid grid-rows-[auto,_1fr] overflow-hidden max-w-5xl m-auto">
         <Nav />
         <main className="grid grid-rows-[auto,_1fr] gap-2 p-4">
           <Router />
         </main>
-      </main>
+      </div>
     </Providers>
   );
 };
